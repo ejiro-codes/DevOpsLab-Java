39d8c582b063030d3b76e24b968dda226de350ca
Ejiro-code <79885301+Ejiro-code@users.noreply.github.com>
2024-04-24 18:55:29 -0700

    java updates new

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -21,7 +21,9 @@ public class NameChecker {
         Matcher m = p.matcher(input);
 
         if (m.find()) {
-            if (pattern.charAt(0) == '-' || pattern.contains("--")) {
+            if (pattern.charAt(0) == '-' || pattern.charAt(0) == '\'' || pattern.contains("--")) {
+                return false;
+            } else {
                 String k = pattern.replaceAll("'", "");
                 if (Math.abs(k.length() - pattern.length()) < 2) {
                     return true;

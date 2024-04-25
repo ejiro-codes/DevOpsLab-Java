7120c8eca8ab854c0c0ef6032f03278e420588a4
Ejiro-code <79885301+Ejiro-code@users.noreply.github.com>
2024-04-24 18:26:22 -0700

    java code

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -1,10 +1,14 @@
 package edu.sjsu;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class NameChecker {
     /**
      * Checks if a given string is a valid name following these rules:
      * - Between 2 to 40 characters
-     * - Contains only alphabetic characters, non-consecutive hyphens, or a single quote
+     * - Contains only alphabetic characters, non-consecutive hyphens, or a single
+     * quote
      * - Cannot start with a hyphen or single quote
      * 
      * @param input Name to check
@@ -12,7 +16,16 @@ public class NameChecker {
      */
     public static boolean check(String input) {
         // TODO: implement
+        String pattern = "^[a-zA-Z][a-zA-Z-']{1,39}$";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(input);
 
+        if (m.find()) {
+            if (pattern.contains("'.*'") == false || pattern.contains("--") == false) {
+                return true;
+            }
+
+        }
         return false;
     }
 }
